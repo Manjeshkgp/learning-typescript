4110390a6f95836c48765df33ac540d5b53cd149
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2023-03-23 14:47:08 +0530

    Type alias learnt

--- app.ts
+++ app.ts
@@ -1,20 +1,45 @@
-let NUM1=1
+// let NUM1=1
 
-function calculate(num1,num2){
-    return num1+num2;
+// function calculate(num1,num2){
+//     return num1+num2;
+// }
+
+// console.log(calculate(NUM1,3))
+
+// function getTotal(numbers:Array<number>){
+//     return numbers.reduce((acc,item)=>{return acc+item},0)
+// }
+
+// console.log(getTotal([1,2,3,4]))
+
+
+// const user = {
+//     firstName:"John",
+//     lastName:"Doe",
+//     role:"React JS Developer"
+// }
+
+
+/* Type alias */
+
+type User = {
+    name:string; // required
+    age:number; // required
+    address?:string // this ? makes this property as optional
 }
 
-console.log(calculate(NUM1,3))
-
-function getTotal(numbers:Array<number>){
-    return numbers.reduce((acc,item)=>{return acc+item},0)
+const user:User = {
+    name:"Mk",
+    age:2,
 }
 
-console.log(getTotal([1,2,3,4]))
-
-
-const user = {
-    firstName:"John",
-    lastName:"Doe",
-    role:"React JS Developer"
+const login = (userData:User):User /*It defines the return type, default is void*/ => {
+    return userData
 }
+
+console.log(login({name:"Sample",age:4,address:"something"}))
+
+
+type ID = number | string;
+
+const UserID:ID = "84r7yfh"
